1ccd6f602904d7064119a708b1de0c567604655c
Kai Zhang <kai@kzhang.org>
2015-04-17 10:51:53 -0700

    add Linkable type class

--- src/Scientific/Workflow/Builder.hs
+++ src/Scientific/Workflow/Builder.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Scientific.Workflow.Builder where
 
 import Control.Arrow (second)
-import Control.Monad.State.Lazy (State, modify)
+import Control.Monad.State.Lazy (State, modify, foldM_)
 import qualified Data.HashMap.Strict as M
 import qualified Data.Text as T
 import Data.Tuple (swap)
@@ -18,17 +19,37 @@ data B = B
 
 type Builder = State B
 
+-- | Declare a computational node
 node :: String -> String -> T.Text -> Builder ()
 node l f anno = modify $ \s -> s{_nodes = (l,f,anno) : _nodes s}
+
+class Linkable a where
+    link :: a -> String -> Builder ()
+
+instance Linkable String where
+    link = link1
+
+instance Linkable (String, String) where
+    link = link2
+
+instance Linkable (String, String, String) where
+    link = link3
+
+-- | Link a node to its parents or inputs
+(~>) :: Linkable a => a -> String -> Builder ()
+(~>) = link
+
+-- | Declare a path. 
+path :: [String] -> Builder ()
+path ns = foldM_ f (head ns) $ tail ns
+  where
+    f a t = link a t >> return t
 
 singleton :: String -> Builder ()
 singleton t = modify $ \s -> s{_links = (t, S t) : _links s}
 
-link :: String -> String -> Builder ()
-link a t = modify $ \s -> s{_links = (t, L a t) : _links s}
-
-(~>) :: String -> String -> Builder ()
-(~>) = link
+link1 :: String -> String -> Builder ()
+link1 a t = modify $ \s -> s{_links = (t, L a t) : _links s}
 
 link2 :: (String, String) -> String -> Builder ()
 link2 (a,b) t = modify $ \s -> s{_links = (t, L2 (a,b) t) : _links s}
