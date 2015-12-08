c8c603e8e819b4ef11ea338519b0fb37cf4a6973
Kai Zhang <kai@kzhang.org>
2015-12-08 14:04:58 -0800

    clean up for v0.4.0

--- src/Scientific/Workflow/Serialization.hs
+++
@@ -1,7 +0,0 @@
-module Scientific.Workflow.Serialization where
-
-import qualified Data.ByteString as B
-
-class Serializable a where
-    serialize :: a -> B.ByteString
-    deserialize :: B.ByteString -> a
