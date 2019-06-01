ff3fb2e49da64e2c669bdf003f28bb5d7b29fb79
Kai Zhang <kai@kzhang.org>
2019-05-31 17:22:59 -0700

    minor

--- SciFlow-app/src/Control/Workflow/Main/Command.hs
+++ SciFlow-app/src/Control/Workflow/Main/Command.hs
@@ -22,7 +22,6 @@ import Control.Workflow.Coordinator
 data SubParser a = SubParser
     { _subparser_name :: String   -- ^ Name of the command.
     , _subparser_desc :: String   -- ^ Description of the command.
-    , _subparser_show :: Bool     -- ^ Whether to show the help text.
     , _subparser      :: Parser a -- ^ Arguments parser.
     }
 
@@ -32,29 +31,33 @@ runParser :: Coordinator coord
 runParser f = SubParser
     { _subparser_name = "run"
     , _subparser_desc = "Run workflow"
-    , _subparser_show = True
     , _subparser      = run f }
 
 viewParser :: SubParser Command
 viewParser = SubParser
     { _subparser_name = "view"
     , _subparser_desc = "Produce HTML visualization of the workflow"
-    , _subparser_show = True
     , _subparser      = view }
 
 remoteParser :: Coordinator coord => Proxy coord -> SubParser Command
 remoteParser proxy = SubParser
     { _subparser_name = "remote"
     , _subparser_desc = "Run workflow in the worker mode"
-    , _subparser_show = False
     , _subparser      = remote proxy }
 
 deleteParser :: SubParser Command
 deleteParser = SubParser
     { _subparser_name = "delete"
     , _subparser_desc = "Delete node cache"
-    , _subparser_show = True
     , _subparser      = delete }
+
+{-
+showParser :: SubParser Command
+showParser = SubParser
+    { _subparser_name = "show"
+    , _subparser_desc = "Show node cache"
+    , _subparser      = show' }
+-}
 
 mkArgsParser :: String   -- ^ Header of the Program helper.
              -> [SubParser a] -> ParserInfo a
@@ -63,10 +66,7 @@ mkArgsParser h cmd = info (helper <*> parser) $ fullDesc <> header h
     parser = subparser $ mconcat $ map mkSubParser cmd
 
 mkSubParser :: SubParser a -> Mod CommandFields a
-mkSubParser SubParser{..} | _subparser_show = base
-                          | otherwise = base <> internal
-  where
-    base = command _subparser_name $ info (helper <*> _subparser) $
-        fullDesc <> progDesc _subparser_desc
+mkSubParser SubParser{..} = command _subparser_name $
+    info (helper <*> _subparser) $ fullDesc <> progDesc _subparser_desc
 {-# INLINE mkSubParser #-}
  
