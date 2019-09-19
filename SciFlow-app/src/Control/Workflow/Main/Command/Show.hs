f2e5c2ee9b7215e97575bebae1a16cb13211e12f
Kai Zhang <kai@kzhang.org>
2019-09-19 15:59:54 -0700

    modify Show

--- SciFlow-app/src/Control/Workflow/Main/Command/Show.hs
+++ SciFlow-app/src/Control/Workflow/Main/Command/Show.hs
@@ -11,41 +11,35 @@ import Data.Binary(Binary)
 import           Options.Applicative
 import Control.Workflow.DataStore (mkKey, withStore, fetchItem, DataStore(..))
 import Control.Workflow.Types
-import Text.Show.Pretty (pPrint)
+import Text.Show.Pretty (ppShow)
 
 import Control.Workflow.Main.Types
 
 data Show' = Show'
-    { jobName :: T.Text
-    , dbPath :: FilePath
-    , raw :: Bool }
+    { dbPath :: FilePath }
 
 instance IsCommand Show' where
     runCommand Show'{..} flow = withStore dbPath $ \store ->
-        runKleisli (showFlow jobName store flow) ()
+        runKleisli (showFlow store flow) ()
 
 show' :: Parser Command
 show' = fmap Command $ Show'
-    <$> strArgument (metavar "NODE")
-    <*> strOption
+    <$> strOption
         ( long "db-path"
        <> value "sciflow.db"
        <> metavar "DB_PATH" )
-    <*> switch
-        ( long "raw"
-       <> help "show raw content. (default: False)" )
 
 showFlow :: Binary env
-         => T.Text
-         -> DataStore
+         => DataStore
          -> SciFlow env
          -> Kleisli IO () ()
-showFlow jn store sciflow = eval (Kleisli . runFlow') $ _flow sciflow
+showFlow store sciflow = eval (Kleisli . runFlow') $ _flow sciflow
   where
     runFlow' (Step job) = runKleisli $ eval ( \(Action _) ->
         Kleisli $ \i -> do
             let key = mkKey i $ _job_name job
             res <- fetchItem store key
-            when (_job_name job == jn) $ pPrint res
+            putStrLn $ show key <> ": " <> ppShow res
+--          when (_job_name job == jn) $ pPrint res
             return res
         ) $ _job_action job
